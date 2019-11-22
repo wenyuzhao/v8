9e6923e40650ef4a4f748883af5f222722c0cad5
Steve Blackburn <steveblackburn@google.com>
2019-11-22 13:58:12 +0100

    compliance

--- src/ic/accessor-assembler.cc
+++ src/ic/accessor-assembler.cc
@@ -85,7 +85,7 @@ TNode<MaybeObject> AccessorAssembler::TryMonomorphicCase(
 
   // Try to quickly handle the monomorphic case without knowing for sure
   // if we have a weak reference in feedback.
-  // TODOO
+  // TODO(steve) map check performance likely an issue here
   GotoIfNot(IsWeakReferenceTo(feedback, lookup_start_object_map), if_miss);
 
   TNode<MaybeObject> handler = UncheckedCast<MaybeObject>(
@@ -121,7 +121,7 @@ void AccessorAssembler::HandlePolymorphicCase(
     TNode<MaybeObject> maybe_cached_map =
         LoadWeakFixedArrayElement(feedback, var_index.value());
     CSA_ASSERT(this, IsWeakOrCleared(maybe_cached_map));
-    // TODO
+    // TODO(steve) map check performance likely an issue here
     GotoIfNot(IsWeakReferenceTo(maybe_cached_map, lookup_start_object_map),
               &loop_next);
 
