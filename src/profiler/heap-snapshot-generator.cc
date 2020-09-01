5a707c76763faf2c5f46b32ed59123ec05ddb5d9
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2020-09-01 20:58:05 +1000

    Fix build error after rebase

--- src/profiler/heap-snapshot-generator.cc
+++ src/profiler/heap-snapshot-generator.cc
@@ -1819,7 +1819,7 @@ class GlobalObjectsEnumerator : public RootVisitor {
   void VisitRootPointersImpl(Root root, const char* description, TSlot start,
                              TSlot end) {
     for (TSlot p = start; p < end; ++p) {
-      DCHECK(!Internals::IsMapWord(p.Relaxed_Load().ptr()));
+      DCHECK(!Internals::IsMapWord(p.Acquire_Load().ptr()));
       Object o = p.load(isolate_);
       if (!o.IsNativeContext(isolate_)) continue;
       JSObject proxy = Context::cast(o).global_proxy();
