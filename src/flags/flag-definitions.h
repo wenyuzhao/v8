5f55ac4fe1b36efba37c2c50d6107df4f7e48232
Wenyu Zhao <wenyu.zhao@anu.edu.au>
2021-05-06 15:09:10 +1000

    Disable move_object_start

--- src/flags/flag-definitions.h
+++ src/flags/flag-definitions.h
@@ -388,6 +388,7 @@ DEFINE_NEG_IMPLICATION(enable_third_party_heap, concurrent_inlining)
 DEFINE_NEG_IMPLICATION(enable_third_party_heap,
                        finalize_streaming_on_background)
 DEFINE_NEG_IMPLICATION(enable_third_party_heap, use_marking_progress_bar)
+DEFINE_NEG_IMPLICATION(enable_third_party_heap, move_object_start)
 
 DEFINE_BOOL_READONLY(enable_third_party_heap, V8_ENABLE_THIRD_PARTY_HEAP_BOOL,
                      "Use third-party heap")
